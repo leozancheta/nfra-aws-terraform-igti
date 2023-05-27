1b887a178f4755476625b550d5ce956c15c1dc5d
Leonardo Candido <leozancheta@gmail.com>
2023-05-27 10:08:55 -0300

    Ajuste nas variaveis

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,15 +1,3 @@
-variable "base_bucket_name" {
-  default = "datalake-lz-igti-producao"
-}
-
-variable "environment" {
-  default = "producao"
-}
-
-variable "acount_number" {
-  default = "865078814163"
-}
-
 variable "region_aws" {
   default = "sa-east-1"
 }
