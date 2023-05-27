499dc1f968ce15238e4f2fef611c2c2b471cbb01
Leonardo Candido <leozancheta@gmail.com>
2023-05-27 11:33:38 -0300

    validação

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,6 +1,6 @@
-resource "aws_s3_bucket_object" "codigo_spark" {
+resource "aws_s3_bucket_object" "job_spark" {
   bucket = aws_s3_bucket.dl.id
-  key    = "glue-code/pyspark/job_spark_glue.py"
+  key    = "emr-code/pyspark/job_spark_from_tf.py"
   acl    = "private"
   source = "../job_spark_glue.py"
   etag   = filemd5("../job_spark_glue.py")
