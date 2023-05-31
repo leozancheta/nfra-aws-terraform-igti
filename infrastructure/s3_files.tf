710989002e81600ad26fcd85ec14d860d6585ffd
Leonardo Candido <leozancheta@gmail.com>
2023-05-30 21:27:50 -0300

    fixed: name bucket

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_object" "job_spark" {
-  bucket = aws_s3_bucket.dl.id
+  bucket = "datalake-lz-igti-producao"
   key    = "emr-code/pyspark/job_spark_from_tf.py"
   acl    = "private"
   source = "../job_spark_glue.py"
